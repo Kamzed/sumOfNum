241bb765f4b6b7c703782d49844693c5f5ff4a63
Aleksandr Chihariov <kamzed1995@gmail.com>
2019-05-04 11:38:21 +0300

    the output field was remote

--- app.js
+++ app.js
@@ -3,11 +3,9 @@
 let inputOne = document.querySelector('[data-element="input-one"]');
 let inputTwo = document.querySelector('[data-element="input-two"]');
 let buttonSum = document.querySelector('[data-element="button-sum"]');
-let outputField = document.querySelector('[data-element="output-field"]');
 
 
 buttonSum.addEventListener('click', () => {
 	let result = (Number(inputOne.value) + Number(inputTwo.value)).toFixed(1);
-	outputField.innerHTML = result;
 	alert(result);
 })
